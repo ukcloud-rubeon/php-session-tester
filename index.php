5cbac823e72a5cce6e3e670c890f439ae32a8c63
Eric Williams <ewilliams@ukcloud.com>
2019-03-01 16:21:50 +0000

    Added limit; changed session name away from sticky name

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
     session_set_cookie_params(3600);
-    session_name("TestSession");
+    session_name("mysession");
     session_start();
     /* increment `views` variable */
     if(isset($_SESSION['views']))
         $_SESSION['views'] = $_SESSION['views']+1;
     else
         $_SESSION['views']=1;
+    if( $_SESSION['views'] >= 10) {
+        /* reset the session? */
+        setcookie("TestSession", "", time() - 3600);
+        setcookie("mysession", "", time() - 3600);
+    };
 ?>
 
 <html>
@@ -15,7 +20,7 @@
 </head>
 <body>
     <h1>Handled by <?php print_r(gethostname()); ?></h1>
-    <p>Session ID: <?php print_r($_COOKIE['TestSession']) ?></p>
+    <p>Session ID: <?php print_r($_COOKIE['mysession']) ?></p>
     <p>Page Views: <?php print_r($_SESSION['views']); ?></p>
 </body>
 </html>
