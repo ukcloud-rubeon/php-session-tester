cd2fe551ea8cc93de7412398ff161d40dae35734
Eric Williams <ewilliams@ukcloud.com>
2019-03-01 09:25:25 +0000

    Added view count

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
     session_set_cookie_params(3600);
     session_name("TestSession");
     session_start();
+    /* increment `views` variable */
+    if(isset($_SESSION['views']))
+        $_SESSION['views'] = $_SESSION['views']+1;
+    else
+        $_SESSION['views']=1;
 ?>
 
 <html>
@@ -10,6 +15,7 @@
 </head>
 <body>
     <h1>Handled by <? print_r(gethostname()); ?></h1>
-    <pre><? print_r($_COOKIE); ?></pre>
+    <p>Session ID: <? print_r($_COOKIE['TestSession']) ?></p>
+    <p>Page Views: <? print_r($_SESSION['views']); ?></p>
 </body>
 </html>
